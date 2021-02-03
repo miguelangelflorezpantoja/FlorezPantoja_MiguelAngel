692595304494322b6b23b361dac160ef33249f69
miguelangelflorezpantoja <miguelangelflorezpantoja@gmail.com>
2021-02-03 11:45:26 -0500

    nueva actualización

--- app/Http/Controllers/libros.php
+++ app/Http/Controllers/libros.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\libro;
-
+use App\Models\editorial;
 class libros extends Controller
 {
     //
@@ -14,4 +14,23 @@ class libros extends Controller
         $libro = DB::table('libro')->get();
         return view('libros.listado',['libro'=>$libro]);
     }
+
+
+    public function formularioReg(){
+        $categorias = editorial::all();
+        return view('libro.form_registro',compact('categorias'));
+    }
+
+    public function registrar( Request $request){
+
+        $producto = new libro ();
+        $producto->nombreProducto = $request->input('nombrepro');
+        $producto->cantidadProducto = $request->input('cantidadpro');
+        $producto->precioProducto = $request->input('preciopro');
+        $producto->fotoProducto = $request->input('fotopro');
+        $producto->categoria = $request->input('categoria');
+        $producto->save();
+        return redirect()->route('listadoProductos');
+    }
+
 }
